a0b00021c4ac6542364a60513a305632ff772870
martingale708 <nyainahr@gmail.com>
2024-08-02 10:02:23 +0200

    correction Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from "../img/argentBankLogo.png"; 
-import "../../src/css/main.css"
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from "../img/argentBankLogo.png";
+import { logout } from "../redux/authSlice";
+import "../sass/header.scss";
 
-const Header = () => {
+function Header () {
+    // Utilisation de useSelector pour vérifier l'état de connexion
+    const isConnected = useSelector((state) => state.auth.token);
+    const firstname = useSelector((state) => state.user.userData.firstname);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const logoutHandler = () => {
+        dispatch(logout());
+        sessionStorage.clear();
+        localStorage.clear();
+        navigate('/');
+    }
+
     return (
-      <nav className="main-nav">
-        <Link className="main-nav-logo" to="/">
-          <img
-            className="main-nav-logo-image"
-            src={logo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </Link>
-        <div>
-          <Link className="main-nav-item" to="/login">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </Link>
-        </div>
-      </nav>
-    );
-  };
-  
-  export default Header;
+        <header>
+            <h1 className='sr-only'>Argent Bank</h1>
+            <nav>
+                <Link to="/">
+                    <img src={Logo} alt="Bank Logo" />
+                </Link> 
+                {isConnected ? (
+                    <div className='connected'>
+                        <Link to='/profile'>
+                            <i className='fa-solid fa-2x fa-circle-user' />
+                            <p>{firstname}</p>
+                        </Link>
+                        <Link to='/' onClick={logoutHandler}>
+                            <i className='fa-solid fa-arrow-right-from-bracket' />
+                            <p> Sign out </p>
+                        </Link>
+                    </div>
+                ) : (
+                    <div className='not-connected'>
+                        <Link to='/login' >
+                            <i className="fa-solid fa-circle-user"></i>
+                            <p>Sign In</p>
+                        </Link>
+                    </div>
+                )}
+            </nav>
+        </header>
+    ) 
+}
+
+export default Header;
